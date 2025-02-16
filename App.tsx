fabcf957a428104e5aa2f90ca75fa4dfecdf858e
Lucas Farias <lucas.farias@rtbhouse.com>
2025-02-15 21:47:12 -0300

    make it use expo-dev-client

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
 
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
+import { OneSignal } from 'react-native-onesignal';
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize('05a5d7f9-2b52-4018-844c-d52eb26f89ca')
+
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
